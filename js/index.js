be6e3f002ad0585f1ac5edeb58a7b0cb3eff64cc
kevinmora47 <kevinmora47@gmail.com>
2020-12-09 19:18:34 -0600

    typo

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ async function chargeHeader() {
     let response = await fetch("https://localhost:5001/api/Header");
     let data = await response.json();
     console.log(data);
-    document.getElementById("legendName").innerHTML = data[0].legendName;                                                                                                                                                                                                                                                                     = data[0].legendName;
+    document.getElementById("legendName").innerHTML = data[0].legendName;                                                                                                                                                                                                                     
     document.getElementById("subLegend").innerHTML = data[0].subLegendName;
     document.getElementById("plans").innerHTML = data[0].plans;
 }
