eae259534b75d1025ba6c76eca80f3f3cf4d2658
kevinmora47 <kevinmora47@gmail.com>
2020-12-09 19:15:05 -0600

    added of fucntion that charges the info header

---
+++ js/index.js
@@ -0,0 +1,9 @@
+chargeHeader();
+async function chargeHeader() {
+    let response = await fetch("https://localhost:5001/api/Header");
+    let data = await response.json();
+    console.log(data);
+    document.getElementById("legendName").innerHTML                                                                                                                                                                                                                                                                           = data[0].legendName;
+    document.getElementById("subLegend").innerHTML = data[0].subLegendName;
+    document.getElementById("plans").innerHTML = data[0].plans;
+}
